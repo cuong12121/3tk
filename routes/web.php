fc29703aee698b2c5f3901b35905b9031d02d631
Cuongdao2222 <cuongdao12121@gmail.com>
2023-08-18 16:33:01 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -96,8 +96,8 @@ Route::get('logout-Fe', 'AjaxController@logout')->name('logout-Fe');
 
 Route::get('sitemap.xml', 'sitemapController@index');
 
-Route::get('sitemap_pc60.xml', 'sitemapController@sitemapChildProduct');
-Route::get('sitemap_pc553.xml', 'sitemapController@sitemapChildProduct');
+// Route::get('sitemap_pc60.xml', 'sitemapController@sitemapChildProduct');
+// Route::get('sitemap_pc553.xml', 'sitemapController@sitemapChildProduct');
 
 
 Route::get('update-order-deal', 'dealController@dealOrder')->name('order-deal');
